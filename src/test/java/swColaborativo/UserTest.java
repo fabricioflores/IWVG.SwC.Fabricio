3e42f439e75f567e46caa054c90df127e955504d
Fabricio Flores <fabrifloresg@gmail.com>
2016-09-22 10:55:44 +0200

    First commit

---
+++ src/test/java/swColaborativo/UserTest.java
@@ -0,0 +1,49 @@
+package swColaborativo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class UserTest {
+	
+	User user;
+	
+    @Before
+    public void before() {
+        user = new User(1, "fabricio", "flores");
+    }
+
+	@Test
+	public void testUser() {
+		assertEquals(1, user.getNumber());
+		assertTrue(user.getName().equals("Fabricio"));
+		assertTrue(user.getFamilyName().equals("Flores"));
+	}
+
+	@Test
+	public void testFullName() {
+		assertTrue(user.fullName().equals("Fabricio Flores"));
+	}
+
+	@Test
+	public void testInitials() {
+		assertTrue(user.initials().equals("F."));
+	}
+
+	@Test
+	public void testgetNumer() {
+		assertEquals(1, user.getNumber());
+	}
+
+	@Test
+	public void testgetName() {
+		assertTrue(user.getName().equals("Fabricio"));
+	}
+
+	@Test
+	public void testgetFamilyName() {
+		assertTrue(user.getFamilyName().equals("Flores"));
+	}
+
+}
